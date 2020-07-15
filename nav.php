b323cfb4f8185e5b0c6addb18b2916f27d4314f5
joundefined <joundefined@users.noreply.github.com>
2020-07-15 14:50:17 +0200

    Nav test 1

--- nav.php
+++ nav.php
@@ -1,44 +1,40 @@
-<!--  Home, Songs, Artists, Playlists and Register / Login -->
-<nav>
-    <a href="index.php">Home | </a><br>
-    <a href="">| Movies Catalogue | </a><br>
-    <a href="">| Manage Catagories | </a><br>
-    <a href="">| Add a Movie | </a><br>
-    <a href="playlists.php">| Playlists | </a><br>
-    <a href="register.php">| Register | </a><br>
-    <a href="login.php">| Log In</a><br>
-
-
-    <input type="text" id="search" placeholder="Search for a movie...">
-
     <?php
 
     if (isset($_POST['logout'])) {
 
         $_SESSION['login'] = false;
         echo 'Bye Bye Birdie<br>';
-        var_dump($_SESSION);
-        var_dump($_COOKIE);
+        // var_dump($_SESSION);
+        // var_dump($_COOKIE);
     }
-
 
     ?>
 
-    <?php if ($_SESSION['login']) : ?>
-        <div>
-            <input type="submit" name="logout" value="Log Out">
-        </div>
-    <?php else : ?>
-        <div>
-            <a href="register.php">
-                <input type="button" name="register" value="Register">
-            </a>
-        </div>
-        <div>
-            <a href="login.php">
-                <input type="button" name="login" value="Log In">
-            </a>
-        </div>
-    <?php endif; ?>
+    <!--  Home, Songs, Artists, Playlists and Register / Login -->
+    <nav>
+        <a href="index.php">Home | </a><br>
+        <a href="">| Movies Catalogue | </a><br>
+        <a href="">| Manage Catagories | </a><br>
+        <a href="">| Add a Movie | </a><br>
+        <a href="playlists.php">| Playlists | </a><br>
 
-</nav>
+
+        <input type="text" id="search" placeholder="Search for a movie...">
+
+
+        <?php if ($_SESSION['login']) : ?>
+            <form action="" method="post">
+                <input type="submit" name="logout" value="Log Out">
+            </form>
+        <?php else : ?>
+            <div>
+                <a href="register.php">| Register | </a><br>
+            </div>
+            <div>
+                <a href="login.php">
+                    <a href="login.php">| Log In</a><br>
+                </a>
+            </div>
+        <?php endif; ?>
+
+    </nav>
