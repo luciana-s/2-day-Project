c96be41a6378cd0a7f6b34125e73115f9d06e1d1
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-07-15 16:11:01 +0200

    ugh not working

--- catalogue.php
+++ catalogue.php
@@ -1,19 +1,3 @@
-<?php
-$connection = mysqli_connect('localhost', 'root', '', 'project_movie');
-if ($connection) {
-    $query = 'SELECT * FROM movies';
-    $result = mysqli_query($connection, $query);
-    $db_records = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    //var_dump($db_records);
-    $movies = json_encode($db_records, JSON_PRETTY_PRINT);
-    //var_dump($movies);
-
-    //var_dump(json_encode($db_records, JSON_PRETTY_PRINT));
-} else {
-    echo 'no connection to the server';
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,35 +22,40 @@ if ($connection) {
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script>
         //getting the array
-        // TODO find a better way 
-        let movies = <?php echo $movies; ?>;
-        console.log(movies);
+        $(function() {
+            $ajax({
+                url: 'datamovies.php',
+                type: 'post',
+                success: createArticles
+            })
 
-        //cloning and making new movie displays
-        const section = document.querySelector('#movies');
-        const model = document.querySelector('#mock');
+        })
 
-        movies.forEach(movie => {
-            //- Duplicate a mockup <article> tag to the <section>
-            const cloneHTML = model.cloneNode(true);
-            section.append(cloneHTML)
-            //- Adding the cat_id as a class for ordering later
-            cloneHTML.classList.add(movie.cat_id);
-            //- title
-            cloneHTML.querySelector('h2').textContent = movie.title;
-            //Movie id
-            cloneHTML.querySelector('span').textContent = movie.movie_id;
-            //Sinopsis
-            cloneHTML.querySelector('p').textContent = movie.sinopis;
-            //- modify the <a> "read more" link (<a>) pointing to the GET movie id
-            cloneHTML.querySelector('a').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
-            //img src
-            cloneHTML.querySelector('img').src = 'imgs/' + movie.poster;
-            // taking out the mock id
-            cloneHTML.id = "";
-        });
-        //removing the mock article
-        model.remove();
+        function createArticles(movies) {
+            //cloning and making new movie displays
+            const section = document.querySelector('#movies');
+            const model = document.querySelector('#mock');
+            movies.forEach(movie => {
+                //- Duplicate a mockup <article> tag to the <section>
+                const cloneHTML = model.cloneNode(true);
+                section.append(cloneHTML)
+                //- Adding the cat_id as a class for ordering later
+                cloneHTML.classList.add(movie.cat_id);
+                //- title
+                cloneHTML.querySelector('h2').textContent = movie.title;
+                //Movie id
+                cloneHTML.querySelector('span').textContent = movie.movie_id;
+                //Sinopsis
+                cloneHTML.querySelector('p').textContent = movie.sinopis;
+                //- modify the <a> "read more" link (<a>) pointing to the GET movie id
+                cloneHTML.querySelector('a').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
+                //img src
+                cloneHTML.querySelector('img').src = 'imgs/' + movie.poster;
+                // taking out the mock id
+                cloneHTML.id = "";
+            });
+            model.remove();
+        }
     </script>
 </body>
 
