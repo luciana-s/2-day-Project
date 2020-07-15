8605426d54788a8510fcb270829305fe6c783b35
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-07-15 16:31:26 +0200

    still not working

--- catalogue.php
+++ catalogue.php
@@ -26,36 +26,35 @@
             $ajax({
                 url: 'datamovies.php',
                 type: 'post',
-                success: createArticles
+                success: function createArticles(movies) {
+                    //cloning and making new movie displays
+                    const section = document.querySelector('#movies');
+                    const model = document.querySelector('#mock');
+                    movies.forEach(movie => {
+                        //- Duplicate a mockup <article> tag to the <section>
+                        const cloneHTML = model.cloneNode(true);
+                        section.append(cloneHTML)
+                        //- Adding the cat_id as a class for ordering later
+                        cloneHTML.classList.add(movie.cat_id);
+                        //- title
+                        cloneHTML.querySelector('h2').textContent = movie.title;
+                        //Movie id
+                        cloneHTML.querySelector('span').textContent = movie.movie_id;
+                        //Sinopsis
+                        cloneHTML.querySelector('p').textContent = movie.sinopis;
+                        //- modify the <a> "read more" link (<a>) pointing to the GET movie id
+                        cloneHTML.querySelector('a').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
+                        //img src
+                        cloneHTML.querySelector('img').src = 'imgs/' + movie.poster;
+                        // taking out the mock id
+                        cloneHTML.id = "";
+                    });
+                    model.remove();
+                }
             })
 
-        })
 
-        function createArticles(movies) {
-            //cloning and making new movie displays
-            const section = document.querySelector('#movies');
-            const model = document.querySelector('#mock');
-            movies.forEach(movie => {
-                //- Duplicate a mockup <article> tag to the <section>
-                const cloneHTML = model.cloneNode(true);
-                section.append(cloneHTML)
-                //- Adding the cat_id as a class for ordering later
-                cloneHTML.classList.add(movie.cat_id);
-                //- title
-                cloneHTML.querySelector('h2').textContent = movie.title;
-                //Movie id
-                cloneHTML.querySelector('span').textContent = movie.movie_id;
-                //Sinopsis
-                cloneHTML.querySelector('p').textContent = movie.sinopis;
-                //- modify the <a> "read more" link (<a>) pointing to the GET movie id
-                cloneHTML.querySelector('a').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
-                //img src
-                cloneHTML.querySelector('img').src = 'imgs/' + movie.poster;
-                // taking out the mock id
-                cloneHTML.id = "";
-            });
-            model.remove();
-        }
+        })
     </script>
 </body>
 
