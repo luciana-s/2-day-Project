6df7cc8db5f89a5f1d73ab6b4351535ce0aafe7c
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-07-16 22:38:16 +0200

    nav css

--- nav.php
+++ nav.php
@@ -9,79 +9,68 @@
     }
 
     ?>
+    <link rel="stylesheet" href="styles/styles.css">
 
-    <style>
-        header {
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 2rem;
-        }
+    <body>
 
-        nav {
-            display: flex;
-        }
+        <!--  Home, Songs, Artists, Playlists and Register / Login -->
+        <header>
+            <nav>
+                <a href="index.php">| Home | </a><br>
+                <a href="catalogue.php">| Movies Catalogue | </a><br>
+                <a href="modify_categorie.php">| Manage Catagories | </a><br>
+                <a href="modify_movie.php">| Manage Movies | </a><br>
+                <a href="playlists.php">| Playlists | </a><br>
+            </nav>
 
-        #userstat {
-            text-align: right;
-        }
-    </style>
-
-    <!--  Home, Songs, Artists, Playlists and Register / Login -->
-    <header>
-        <nav>
-            <a href="index.php">| Home | </a><br>
-            <a href="catalogue.php">| Movies Catalogue | </a><br>
-            <a href="modify_categorie.php">| Manage Catagories | </a><br>
-            <a href="modify_movie.php">| Manage Movies | </a><br>
-            <a href="playlists.php">| Playlists | </a><br>
-        </nav>
-
-        <form action="" method="post">
-            <input type="text" name="mysearch1" placeholder="Search for a movie...">
-            <input type="submit" name="search1" value="find movies">
-        </form>
-
-        <?php if (!isset($_SESSION['login']) || $_SESSION['login'] == false) : ?>
-            <div>
-                <a href="register.php">| Register |</a>
-                <a href="login.php">| Log In |</a>
-            </div>
-        <?php elseif ($_SESSION['login']) : ?>
-            <form id="userstat" action="" method="post">
-                <input type="submit" name="logout" value="Log Out"><br>
-                <div>Logged In User: <?= $_SESSION['sessUser'] ?></div>
-                <div>User email: <?= $_SESSION['email'] ?></div>
+            <form action="" method="post" id="search1">
+                <input type="text" name="mysearch1" placeholder="Search for a movie..." id="searchInput1">
+                <input type="submit" name="search1" value="find movies">
             </form>
-        <?php endif; ?>
-    </header>
 
-    <p>
-        <?php
+            <?php if (!isset($_SESSION['login']) || $_SESSION['login'] == false) : ?>
+                <div>
+                    <a href="register.php">| Register |</a>
+                    <a href="login.php">| Log In |</a>
+                </div>
+            <?php elseif ($_SESSION['login']) : ?>
+                <form id="userstat" action="" method="post">
+                    <input type="submit" name="logout" value="Log Out"><br>
+                    <div>Logged In User: <?= $_SESSION['sessUser'] ?></div>
+                    <div>User email: <?= $_SESSION['email'] ?></div>
+                </form>
+            <?php endif; ?>
+        </header>
 
-        if (isset($_POST['search1'])) {
-            // clean the string
-            $searchTitle = trim($_POST['mysearch1']);
+        <p>
+            <?php
 
-            // open a connection to DB
-            $conn = mysqli_connect('localhost', 'root', '', 'project_movie');
+            if (isset($_POST['search1'])) {
+                // clean the string
+                $searchTitle = trim($_POST['mysearch1']);
 
-            $query = "SELECT * FROM movies WHERE title LIKE '%$searchTitle%'";
+                // open a connection to DB
+                $conn = mysqli_connect('localhost', 'root', '', 'project_movie');
 
-            // send SQL request to DB
-            $result_query = mysqli_query($conn, $query);
+                $query = "SELECT * FROM movies WHERE title LIKE '%$searchTitle%'";
 
-            $foundmovies = mysqli_fetch_all($result_query, MYSQLI_ASSOC);
-            // var_dump($foundmovies);
+                // send SQL request to DB
+                $result_query = mysqli_query($conn, $query);
+
+                $foundmovies = mysqli_fetch_all($result_query, MYSQLI_ASSOC);
+                // var_dump($foundmovies);
 
 
-            foreach ($foundmovies as $key => $value) {
-                echo '<img src="imgs/' . $value['poster'] . '" width="150px" alt=""><br>';
-                echo $value['title'] . '<br>';
+                foreach ($foundmovies as $key => $value) {
+                    echo '<img src="imgs/' . $value['poster'] . '" width="150px" alt=""><br>';
+                    echo $value['title'] . '<br>';
+                }
+
+                // CLOSE the resource/connection
+                mysqli_close($conn);
             }
 
-            // CLOSE the resource/connection
-            mysqli_close($conn);
-        }
+            ?>
+        </p>
 
-        ?>
-    </p>
+    </body>
