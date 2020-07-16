04132e5f799671189e8dfb8e6928b9aaca3b2f54
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-07-16 11:31:09 +0200

    form

--- modify_movie.php
+++ modify_movie.php
@@ -1,10 +1,12 @@
 <?php
+require_once 'nav.php';
 $connection = mysqli_connect('localhost', 'root', '', 'project_movie');
 if ($connection) {
     if (isset($_GET['id'])) {
         $urlID = $_GET['id'];
         $queryURL = "SELECT * FROM movies WHERE movie_id = $urlID";
         $resultURL = mysqli_query($connection, $queryURL);
+        $db_record = mysqli_fetch_assoc($resultURL);
     } else {
         header('Location: http://localhost/Project/2-day-Project/catalogue.php');
     }
@@ -22,7 +24,14 @@ if ($connection) {
 </head>
 
 <body>
-
+    <form action="">
+        <input type="number" name="id" value="<?php $db_record['movie_id']; ?>"><br>
+        <input type="text" name="" value="<?php $db_record['title']; ?>"><br>
+        <input type="date" name="" value="<?php $db_record['year_of_release']; ?>"><br>
+        <input type="text" name="" value="<?php $db_record['poster']; ?>"><br>
+        <input type="text" name="" value="<?php $db_record['path']; ?>"><br>
+        <input type="textarea" name="" value="<?php $db_record['sinopsis']; ?>"><br>
+    </form>
 </body>
 
 </html>
