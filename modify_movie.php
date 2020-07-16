ec2c3e95d22c00fc7bb3c99ad594fe60b803ae59
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-07-16 11:09:32 +0200

    lil thing

--- modify_movie.php
+++ modify_movie.php
@@ -5,6 +5,8 @@ if ($connection) {
         $urlID = $_GET['id'];
         $queryURL = "SELECT * FROM movies WHERE movie_id = $urlID";
         $resultURL = mysqli_query($connection, $queryURL);
+    } else {
+        header('Location: http://localhost/Project/2-day-Project/catalogue.php');
     }
 } else {
     echo 'no connection to the server';
@@ -16,7 +18,7 @@ if ($connection) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Modify movie</title>
 </head>
 
 <body>
