4445d706351549a8545128168366c4155c43d2f4
luciana-s <65956715+luciana-s@users.noreply.github.com>
2020-07-16 09:21:35 +0200

    some css

--- catalogue.php
+++ catalogue.php
@@ -5,19 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catalogue</title>
+    <link rel="stylesheet" href="styles/catalogue/catalogue.css">
 </head>
 
 <body>
     <nav id="order">
-
+    <button>d</button>
     </nav>
     <section id="movies">
         <article id="mock">
             <img src="" alt="">
-            <span></span>
-            <h2></h2>
-            <p></p>
-            <a href="">...See more</a>
+            <div class="column">
+                <div class="id_and_title">
+                    <h2 class="id"></h2>
+                    <h2 class="title"></h2>
+                </div>
+                <p class="sinopsis"></p>
+            </div>
+            <div class="links">
+                <a href="" class="more">...See more</a>
+                <a href="" class="modify">Modify</a>
+            </div>
         </article>
     </section>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -46,13 +54,15 @@
                 //- Adding the cat_id as a class for ordering later
                 cloneHTML.classList.add(movie.cat_id);
                 //- title
-                cloneHTML.querySelector('h2').textContent = movie.title;
+                cloneHTML.querySelector('.title').textContent = movie.title;
                 //Movie id
-                cloneHTML.querySelector('span').textContent = movie.movie_id;
+                cloneHTML.querySelector('.id').textContent = '#' + movie.movie_id + ' ';
                 //Sinopsis
-                cloneHTML.querySelector('p').textContent = movie.sinopis;
+                cloneHTML.querySelector('.sinopsis').textContent = movie.sinopsis;
+                //- modify the <a> to the GET movie id
+                cloneHTML.querySelector('.more').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
                 //- modify the <a> "read more" link (<a>) pointing to the GET movie id
-                cloneHTML.querySelector('a').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
+                cloneHTML.querySelector('.modify').href = 'http://localhost/Project/2-day-Project/catalogue.php?id=' + movie.movie_id;
                 //img src
                 cloneHTML.querySelector('img').src = 'imgs/' + movie.poster;
                 // taking out the mock id
